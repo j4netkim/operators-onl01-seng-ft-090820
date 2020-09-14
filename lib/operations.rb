4c2662aded4796557026e86a9b28b01c37035357
J K <kj688895@gmail.com>
2020-09-14 20:52:08 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -8,12 +8,6 @@ end
 
 
 
-def not_safe?(speed)
-	if speed > 60 || speed < 40
-		return true
-	else
-		return false
-	end
-end
 
-speed > 60 || speed < 40 ? return true : return false 
+
+speed > 60 || speed < 40 ? return true : return false
