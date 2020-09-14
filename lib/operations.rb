80b92f3da823b811d7bd34bbeb794861c6c1c4f7
J K <kj688895@gmail.com>
2020-09-14 20:25:21 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,18 +1,11 @@
 def unsafe?(speed)
-	if speed < 40
-		return "true"
-	elsif speed > 60
-		return "true"
-	else 
-		return "false"
+	if 40 > speed > 60
+		return true
 	end
 end
 
 
 
 def not_safe?(speed)
-	
+
 end
-	
-
-
